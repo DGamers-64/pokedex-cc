c6243e1dac82a006d031087e4c1fa5c23ff8eec9
DGamers-64 <dgamers.643@gmail.com>
2025-10-05 17:06:18 +0200

    Últimos cambios y retoques a la lógica del código

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
     document.querySelector("button").addEventListener("click", generarPokemon)
     document.getElementById("pokemonInput").addEventListener("keydown", (e) => {
         if (e.key == "Enter") generarPokemon(e)
@@ -8,47 +7,66 @@ document.addEventListener("DOMContentLoaded", () => {
 
 async function generarPokemon(e) {
     const pokemonInput = document.getElementById("pokemonInput")
-    const pokemon = document.getElementById("pokemon")
+    const pokemonContainer = document.getElementById("pokemon")
+    const errorContainer = document.getElementById("error")
+    const bienvenidaContainer = document.getElementById("bienvenida")
+    const statsContainer = document.querySelectorAll(".estadistica")
     e.preventDefault()
+
+    let pokemon = {}
+    let pokemonSpecies = {}
+    let pokemonEncontrado = false
     
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput.value}`)
-        .then(res => res.json())
-        .then(data => { 
-            const nombre = data.forms[0].name
-            const stats = data.stats
-            const cries = data.cries
-
-            document.getElementById("nombrePokemon").innerHTML = nombre[0].toUpperCase() + nombre.substring(1)
-            document.getElementById("imagenPokemon").src = data.sprites.versions['generation-ii'].crystal.front_default
-            document.getElementById("imagenPokemonShiny").src = data.sprites.versions['generation-ii'].crystal.front_shiny
-
-            document.getElementById("estadistica-vida").innerHTML = stats.find(e => e.stat.name == "hp").base_stat
-            document.getElementById("estadistica-ataque").innerHTML = stats.find(e => e.stat.name == "attack").base_stat
-            document.getElementById("estadistica-ataque-especial").innerHTML = stats.find(e => e.stat.name == "special-attack").base_stat
-            document.getElementById("estadistica-defensa").innerHTML = stats.find(e => e.stat.name == "defense").base_stat
-            document.getElementById("estadistica-defensa-especial").innerHTML = stats.find(e => e.stat.name == "special-defense").base_stat
-            document.getElementById("estadistica-velocidad").innerHTML = stats.find(e => e.stat.name == "speed").base_stat
-
-            document.getElementById("audio-pokemon-latest").src = cries.latest
-            document.getElementById("audio-pokemon-legacy").src = cries.legacy
-
-        })
-        .catch(e => {
-            console.error(`Hubo un error inesperado: ${e}`)
-        })
-    
-    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonInput.value}`)
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonInput.value}`)
         .then(res => res.json())
         .then(data => {
-            document.querySelector("main").style.backgroundColor = data.color.name
-            document.querySelectorAll("th").forEach(e => {
-                e.style.backgroundColor = data.color.name
-            })
+            pokemon = data
+            pokemonEncontrado = true
         })
         .catch(e => {
-            alert("¡No ha sido encontrado el pokémon!")
+            console.error(`Hubo un error inesperado: ${e}`)
+        })
+    
+    await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonInput.value}`)
+        .then(res => res.json())
+        .then(data => {
+            pokemonSpecies = data
+            pokemonEncontrado = true
+        })
+        .catch(e => {
             console.error(`Hubo un error inesperado: ${e}`)
         })
 
-    pokemon.style.display = "flex"
+    if (pokemonEncontrado) {
+        document.getElementById("nombrePokemon").innerHTML = pokemon.name[0].toUpperCase() + pokemon.name.substring(1)
+        document.getElementById("imagenPokemon").src = pokemon.sprites.other["official-artwork"].front_default
+        document.getElementById("imagenPokemonShiny").src = pokemon.sprites.other["official-artwork"].front_shiny
+    
+        pokemon.stats.forEach((e, i) => {
+            statsContainer[i].textContent = e.base_stat
+        })    
+    
+        document.getElementById("audio-pokemon-latest").src = pokemon.cries.latest
+    
+        const colorPokemon = pokemonSpecies.color.name
+    
+        document.getElementsByTagName("main")[0].style.backgroundColor = colorPokemon
+    
+        document.querySelectorAll("th").forEach(e => {
+            e.style.backgroundColor = colorPokemon
+            if (colorPokemon == "black") {
+                e.style.color = "white"
+            } else {
+                e.style.color = "black"
+            }
+        })
+    
+        pokemonContainer.style.display = "flex"
+        errorContainer.style.display = "none"
+    } else {
+        pokemonContainer.style.display = "none"
+        errorContainer.style.display = "flex"
+    }
+
+    bienvenidaContainer.style.display = "none"
 }
